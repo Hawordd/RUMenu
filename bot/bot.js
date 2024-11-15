e9ee05d38d3c239fa2e71f7f50951bf1d05a14da
Hawordd <matthisgastesoyer@gmail.com>
2024-11-15 14:17:29 +0100

    Moved sendDailyMenu & prepared stopmenu command

--- bot/bot.js
+++ bot/bot.js
@@ -3,21 +3,19 @@ import { sendMenu } from './commands/menu.js';
 import { loadChannels, channelIntegration } from './commands/setchannel.js';
 import cron from 'node-cron';
 import dotenv from 'dotenv';
-import { fetchMenu } from './utils/fetchMenu.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { loadMenus, saveMenu, createMenuEmbed } from './commands/menu.js';
+import { sendDailyMenu } from './utils/sendDailyMenu.js';
+import { stopMenu } from './commands/stopmenu.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const TOKEN = process.env.DISCORD_BOT_TOKEN;
-
 
 client.once(Events.ClientReady, async () => {
     console.log(`Connecté en tant que ${client.user.tag}`);
@@ -32,6 +30,10 @@ client.once(Events.ClientReady, async () => {
             {
                 name: 'menu',
                 description: 'Affiche le menu du jour du RU Aubépin'
+            },
+            {
+                name: 'stopmenu',
+                description: 'Arrête l\'envoi du menu quotidien'
             }
         ]);
         console.log('Commandes synchronisées.');
@@ -49,47 +51,18 @@ client.once(Events.ClientReady, async () => {
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    const channels = loadChannels();
-
     if (interaction.commandName === 'setchannel') {
+        const channels = loadChannels();
         await channelIntegration(interaction, channels);
     }
 
     if (interaction.commandName === 'menu') {
         await sendMenu(interaction);
     }
+
+    if (interaction.commandName === 'stopmenu') {
+        await stopMenu(interaction);
+    }
 });
-
-async function sendDailyMenu() {
-    const channels = loadChannels();
-    const date = new Date().toLocaleDateString();
-    const menus = loadMenus();
-    let menu = null;
-
-    if (menus[date]) {
-        console.log('Menu déjà enregistré pour aujourd\'hui.');
-        menu = menus[date];
-    } else {
-        console.log('Aucun menu trouvé pour aujourd\'hui, récupération...');
-        menu = await fetchMenu();
-        saveMenu(date, menu);
-    }
-
-    const embed = createMenuEmbed(menu);
-
-    for (const guildId in channels) {
-        const channelId = channels[guildId];
-        const channel = await client.channels.fetch(channelId);
-        if (channel) {
-            if (menu) {
-                await channel.send({ embeds: [embed] });
-            } else {
-                console.error('Aucun menu trouvé.');
-            }
-        } else {
-            console.error(`Erreur : le canal ${channelId} pour la guilde ${guildId} est introuvable.`);
-        }
-    }
-}
 
 client.login(TOKEN);
---
+++ bot/utils/sendDailyMenu.js
@@ -0,0 +1,34 @@
+import { fetchMenu } from './fetchMenu.js';
+import { loadMenus, saveMenu, createMenuEmbed } from '../commands/menu.js';
+
+export async function sendDailyMenu() {
+    const channels = loadChannels();
+    const date = new Date().toLocaleDateString();
+    const menus = loadMenus();
+    let menu = null;
+
+    if (menus[date]) {
+        console.log('Menu déjà enregistré pour aujourd\'hui.');
+        menu = menus[date];
+    } else {
+        console.log('Aucun menu trouvé pour aujourd\'hui, récupération...');
+        menu = await fetchMenu();
+        saveMenu(date, menu);
+    }
+
+    const embed = createMenuEmbed(menu);
+
+    for (const guildId in channels) {
+        const channelId = channels[guildId];
+        const channel = await client.channels.fetch(channelId);
+        if (channel) {
+            if (menu) {
+                await channel.send({ embeds: [embed] });
+            } else {
+                console.error('Aucun menu trouvé.');
+            }
+        } else {
+            console.error(`Erreur : le canal ${channelId} pour la guilde ${guildId} est introuvable.`);
+        }
+    }
+}
